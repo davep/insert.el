0b87621131bd3d2b7bee2986eea248f5bd8603df
Dave Pearson <davep@davep.org>
2017-07-19 09:26:18 +0100

    v1.8

--- insert.el
+++ insert.el
@@ -2,7 +2,7 @@
 ;; Copyright 2017 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.7
+;; Version: 1.8
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/insert.el
 
